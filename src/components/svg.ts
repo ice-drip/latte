b950c0f6e1114340f285eeb07eae59ba9f086767
rikka <admin@rikka.cc>
2022-06-22 15:21:32 +0800

    fix: 🐛 删除多余报错

--- src/components/svg.ts
+++ src/components/svg.ts
@@ -3,40 +3,51 @@ interface SVGItem {
   literal: string;
   iconName: string;
   viewBox: string;
-  height:number;
-  width:number;
+  height: number;
+  width: number;
 }
 
 const Svg = (svgList: SVGItem[]) =>
   class SvgComponent extends HTMLElement {
     public readonly svgList: SVGItem[] = svgList;
+
+    static get observedAttributes() {
+      return ["name", "namespace", "width", "height"];
+    }
+
     connectedCallback() {
       this.initHTML();
     }
 
-    attributeChangedCallback(name: string, _oldValue: string, _newValue: string) {
+    attributeChangedCallback(
+      name: string,
+      _oldValue: string,
+      _newValue: string
+    ) {
       const attrs = new Set(["namespace", "name", "width", "height"]);
       if (attrs.has(name)) {
         this.initHTML();
       }
     }
 
-    initHTML(){
+    initHTML() {
       const namespace = this.getAttribute("namespace") ?? "default";
       const iconName = this.getAttribute("name");
       const width = this.getAttribute("width");
       const height = this.getAttribute("height");
-      
-      if (iconName) {
-        const svg = this.svgList.find((icon) => icon.iconName === iconName && icon.namespace === namespace);
+
+      if (iconName && namespace && width && height) {
+        const svg = this.svgList.find(
+          (icon) => icon.iconName === iconName && icon.namespace === namespace
+        );
         if (svg) {
-          const style = width&&height?`width:${width};height:${height};`:`width:${svg.width}px;height:${svg.height}px;`;
+          const style =
+            width && height
+              ? `width:${width};height:${height};`
+              : `width:${svg.width}px;height:${svg.height}px;`;
           this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="${style}" viewBox="${svg.viewBox}">${svg.literal}</svg>`;
         }
-      }else{
-        throw new Error("SVG name is required");
       }
     }
-
   };
-export { Svg,SVGItem };
+export { Svg, SVGItem };
